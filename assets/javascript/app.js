952205729494f54162fa125b4bce4bb683ffa7c9
sajad1993gh <sajad1993gh@gmail.com>
2019-06-08 23:49:33 -0400

    adding basics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(event) {
 
-    var key = Wohrfl96PkbUD9MWz44E12WzGd9ODdrD;
-    var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=" + key;
+    // var key = Wohrfl96PkbUD9MWz44E12WzGd9ODdrD;
+    var queryURLbase = "https://api.giphy.com/v1/gifs/search?api_key=Wohrfl96PkbUD9MWz44E12WzGd9ODdrD";
 
 
 
